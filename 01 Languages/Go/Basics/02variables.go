b600e1497ec0c2d12120cea22bc88496a3eb29da
Sahil Naik <nsahil992@gmail.com>
2023-12-28 19:20:20 +0530

    Variables added

--- 01 Languages/Go/Basics/02variables.go
+++ 01 Languages/Go/Basics/02variables.go
@@ -1,12 +1,12 @@
 package main
 import ("fmt")
 func main(){
-	var a int = 99
+	var a int = 99  // we declared variable 'a' by using var keyword which is of type int and assigned 99 to it.
 	var b int = 1
 	var c = a + b
 
-	x := 111111111
+	x := 111111111  // := short variable declaration assignment
 	y := 111111111
-	fmt.Println(c)
-	fmt.Println(x * y)
+	fmt.Println(c)  // prints the sum of a + b
+	fmt.Println(x * y) 
 }
