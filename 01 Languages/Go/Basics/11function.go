62a2e7f69645babe4d4dedea24740fc46a6e235c
Sahil Naik <nsahil992@gmail.com>
2024-01-02 19:21:41 +0530

    Functions added

--- 01 Languages/Go/Basics/11function.go
+++ 01 Languages/Go/Basics/11function.go
@@ -4,6 +4,16 @@ import("fmt")
 func greeting(a string) {
 	fmt.Println("Hey there,",a)
 }
+
+// Return type functions
+
+func operations(a int, b int) (sum int, diff int) {
+	sum = a + b
+	diff = a - b
+	return
+}
 func main() {
 	greeting("Sahil")
+	sum, diff := operations(10, 9)
+	println(sum," ",diff)
 }
