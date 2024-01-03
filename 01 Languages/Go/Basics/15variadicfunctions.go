52679e266e55099c00a5805d3e560b85f8ec07bf
Sahil Naik <nsahil992@gmail.com>
2024-01-03 18:41:11 +0530

    Variadic Functions added

--- 01 Languages/Go/Basics/15variadicfunctions.go
+++ 01 Languages/Go/Basics/15variadicfunctions.go
@@ -9,11 +9,21 @@ func sumNumbers(numbers ...int)int {
 	return sum
 }
 
+	// Eg with more than one parameter
+
+func printDetails(student string, subjects ...string) {
+	fmt.Println("Hey", student, ", here are your subjects - ")
+	for _, sub := range subjects {
+		fmt.Printf("%s, ",sub)
+	}
+}
+
 func main() {
 	fmt.Println(sumNumbers())
 	fmt.Println(sumNumbers(10))
 	fmt.Println(sumNumbers(10, 20))
-	fmt.Println(sumNumbers(10, 20, 30, 40, 50))
+	fmt.Println(sumNumbers(10, 20, 30, 40, 50))	
 
-	
+
+	printDetails("Sahil", "Quantum Physics", "Rocket Science")
 }
