15520b389d8b71f49f0bdd75cf6d7999703babfe
Sahil Naik <nsahil992@gmail.com>
2024-01-03 18:35:07 +0530

    Variadic Functions added

---
+++ 01 Languages/Go/Basics/15variadicfunctions.go
@@ -0,0 +1,19 @@
+package main
+import ("fmt")
+
+func sumNumbers(numbers ...int)int {
+	sum := 0
+	for _, value := range numbers {
+		sum += value
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(sumNumbers())
+	fmt.Println(sumNumbers(10))
+	fmt.Println(sumNumbers(10, 20))
+	fmt.Println(sumNumbers(10, 20, 30, 40, 50))
+
+	
+}
