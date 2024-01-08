0f0e430ce0d88084b52bc3cfe9939c1600fbcb4c
Sahil Naik <nsahil992@gmail.com>
2024-01-08 19:28:13 +0530

    Userinput added

--- 01 Languages/Go/Basics/04userinput.go
+++ 01 Languages/Go/Basics/04userinput.go
@@ -2,12 +2,12 @@ package main
 import ("fmt")
 
 func main(){
-	var a string 
-	var b int 
+	var a string // String datatype
+	var b int   //  Int datatype
 	fmt.Println("Enter a string and a number: ")
-	count, err := fmt.Scanf("%s %d", &a, &b)
-	fmt.Println("count: ", count)
-	fmt.Println("error: ", err)
+	count, err := fmt.Scanf("%s %d", &a, &b) // takes user input for a and b
+	fmt.Println("count: ", count)           // Checks the count
+	fmt.Println("error: ", err)            // Checks the error if user provided invalid datatype 
 	fmt.Println("a: ", a)
 	fmt.Println("b: ", b)
 
