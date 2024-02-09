82661eef3eb790c679eb342539c70eae28fe6e7d
Sahil Naik <nsahil992@gmail.com>
2024-02-09 19:59:43 +0530

    feat: go routines

--- 01 Languages/Go/Advanced/Concurrency/02goroutines.go
+++ 01 Languages/Go/Advanced/Concurrency/02goroutines.go
@@ -13,9 +13,10 @@ func main() {
 
 	start := time.Now()
 	for i := 1; i <= 10000; i++ {
-		go calculateSquare(i)
+		go calculateSquare(i)   // go keyword is used when calling a function
+		// Here all these 10000 function calls will be printed in 10000 go routines
 	}
 	elapsed := time.Since(start)
-	time.Sleep(2 * time.Second)
+	time.Sleep(2 * time.Second) // our main function is going to wait atleast 2 seconds before it exits
 	fmt.Println("Function took", elapsed)
 }
