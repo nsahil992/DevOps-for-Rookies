07a4d8e6940ce482f5cc5a0372ac1eb8fe721823
Sahil Naik <nsahil992@gmail.com>
2024-09-20 22:34:27 +0530

    Update 01helloworld.go

--- 01 Languages/Go/Basics/01helloworld.go
+++ 01 Languages/Go/Basics/01helloworld.go
@@ -1,4 +1,4 @@
-package main // this line tells julia that 01helloworld.go contains a package called main
+package main // this line tells go that 01helloworld.go contains a package called main
 import ("fmt") // we import a module with a more powerful printing function (fmt stands for format)
 
 func main(){ // this is a main function which acts as a entrypoint
